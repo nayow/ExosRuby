102084c20c53b8f3d31c01ae1ae932a1d2ccb3b0
nayow <nayow@hotmail.fr>
2020-01-15 09:31:44 +0100

    a

--- exo_06.rb
+++ exo_06.rb
@@ -5,4 +5,4 @@ number_of_weeks_in_THP = 11
 puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
 puts "Et en minutes ça fait : #{number_of_minutes_in_an_hour * number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
 
-#affiche le résultat du calcul au sein d'une chaine grace aux variables
+#erreur car number_of_minutes_in_an_hour n'a pas été défini
